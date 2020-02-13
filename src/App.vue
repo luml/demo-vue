8409d583dfa1cdd78bb2bc55dedab6abd2e4ace1
Elio <elio@Elios-iMac.local>
2020-02-13 21:40:25 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to My First Demo Vue.js App"/>
+    <ListItems msg="App Content List"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import ListItems from './components/ListItems'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    ListItems
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ListItems.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="list">
+        <h3>{{msg}}</h3>
+        <ul v-for="count in 5" :key="count">
+            <li>
+                {{ count }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'ListItems',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
