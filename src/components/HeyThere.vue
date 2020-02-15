45c53f7c5d2db74ce9f1e2db66ccaac03688a44b
lumeiling <1520622848@qq.com>
2020-02-15 17:09:06 +0800

    imply v-on:click(but error)

--- src/components/HeyThere.vue
+++ src/components/HeyThere.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="hey">
     <h3>Nice to meet you, {{name}}</h3>
-    <input v-model="name">
+    <input v-model="name" />
     <h4>**Love might happen in the most unexcepted place and time**</h4>
+    <div>
+      <button v-on:click="disagree">Disagree</button>
+      <code>{{ opinion }}</code>
+      <button v-on:click="agree">Agree</button>
+    </div>
   </div>
 </template>
 
@@ -10,14 +15,34 @@
 export default {
   name: "HeyThere",
   props: {
-    name: String
+    name: String,
+    opinion: String,
+    methods: {
+      disagree: function() {
+        this.opinion = "You think?"
+      },
+      agree: function() {
+        this.opinion = "You kidding!"
+      }
+    }
   }
 };
 </script>
 
 <style scoped>
+h3 {
+  margin-bottom: 0;
+}
 h4 {
-  color: deepPink;
+  color: #42b983;
   font-style: italic;
+}
+input {
+  margin-top: 10px;
+  padding: 0.3rem;
+}
+button {
+  border-radius: 5px;
+  width: 150px;
 }
 </style>
