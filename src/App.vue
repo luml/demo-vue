91dc6dbb57b05edd8875dd036def643b96d95498
luml <1520622848@qq.com>
2020-05-24 13:37:00 +0800

    feat(computed prop for light dark mode toggle)

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <div id="app">
+  <div id="app" :class="modeClass">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HelloWorld msg="Welcome to the simplest Demo Vue.js App" />
     <ListItems msg="App Content Docs" arr="ABC" buttons="abc" alert="I warn you Jimmy!" />
     <HeyThere person="Jimmy" />
+    <label class="checkbox">
+      <input type="checkbox" v-model="darkMode">
+      Dark Mode
+    </label>
   </div>
 </template>
 
@@ -18,7 +22,17 @@ export default {
     HelloWorld,
     ListItems,
     HeyThere
-  }
+  },
+  data() {
+    return {
+      darkMode: false
+    }
+  },
+  computed: {
+    modeClass(){
+      return this.darkMode ? 'dark-mode' : 'light-mode';
+    }
+  },
 };
 </script>
 
@@ -31,7 +45,15 @@ body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+/* .light-mode h1, h3, p {
+  color: black;
+} */
+.dark-mode {
+  background: #000;
+  color: #bbb;
+}
+.dark-mode h1, h3, p {
+  color: #bbb;
 }
 </style>
